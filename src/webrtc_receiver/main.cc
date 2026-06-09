1e8bef111875a7a5a6720d8f4ba014787e806a1a
Feiguaaa <JinxR_@outlook.com>
2026-06-09 17:22:18 +0800

    init

---
+++ src/webrtc_receiver/main.cc
@@ -0,0 +1,367 @@
+/*
+ *  Copyright 2024 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/memory/memory.h"
+#include "api/audio_codecs/builtin_audio_decoder_factory.h"
+#include "api/audio_codecs/builtin_audio_encoder_factory.h"
+#include "api/create_modular_peer_connection_factory.h"
+#include "api/enable_media.h"
+#include "api/environment/environment.h"
+#include "api/environment/environment_factory.h"
+#include "api/field_trials.h"
+#include "api/jsep.h"
+#include "api/make_ref_counted.h"
+#include "api/media_stream_interface.h"
+#include "api/peer_connection_interface.h"
+#include "api/rtc_error.h"
+#include "api/rtp_receiver_interface.h"
+#include "api/scoped_refptr.h"
+#include "api/units/time_delta.h"
+#include "api/video/video_frame.h"
+#include "api/video/video_sink_interface.h"
+#include "api/video/video_source_interface.h"
+#include "api/video_codecs/video_decoder_factory_template.h"
+#include "api/video_codecs/video_decoder_factory_template_dav1d_adapter.h"
+#include "api/video_codecs/video_decoder_factory_template_libvpx_vp8_adapter.h"
+#include "api/video_codecs/video_decoder_factory_template_libvpx_vp9_adapter.h"
+#include "api/video_codecs/video_decoder_factory_template_open_h264_adapter.h"
+#include "api/video_codecs/video_encoder_factory_template.h"
+#include "api/video_codecs/video_encoder_factory_template_libaom_av1_adapter.h"
+#include "api/video_codecs/video_encoder_factory_template_libvpx_vp8_adapter.h"
+#include "api/video_codecs/video_encoder_factory_template_libvpx_vp9_adapter.h"
+#include "api/video_codecs/video_encoder_factory_template_open_h264_adapter.h"
+#include "examples/peerconnection/client/defaults.h"
+#include "examples/peerconnection/client/flag_defs.h"
+#include "examples/peerconnection/client/peer_connection_client.h"
+#include "examples/peerconnection/headless_common/headless_socket_server.h"
+#include "examples/peerconnection/headless_common/receiver_sink.h"
+#include "examples/peerconnection/headless_common/signaling_helper.h"
+#include "json/reader.h"
+#include "json/writer.h"
+#include "rtc_base/logging.h"
+#include "rtc_base/ssl_adapter.h"
+#include "rtc_base/strings/json.h"
+#include "rtc_base/thread.h"
+
+ABSL_FLAG(std::string, output_file, "received.y4m",
+          "Path to write the received Y4M video.");
+ABSL_FLAG(bool, play, false,
+          "Play received video in real-time using ffplay "
+          "(ignores --output_file).");
+
+namespace {
+
+class Receiver : public webrtc::PeerConnectionObserver,
+                 public webrtc::CreateSessionDescriptionObserver,
+                 public PeerConnectionClientObserver {
+ public:
+  Receiver(const webrtc::Environment& env,
+           PeerConnectionClient* client,
+           const std::string& output_file,
+           bool play)
+      : env_(env), client_(client) {
+    if (play) {
+      FILE* pipe = popen("ffplay -f yuv4mpegpipe -i pipe:0:", "w");
+      if (!pipe) {
+        RTC_LOG(LS_ERROR) << "Failed to start ffplay. Is it installed?";
+        y4m_sink_ = std::make_unique<Y4mVideoSink>(output_file);
+      } else {
+        y4m_sink_ = std::make_unique<Y4mVideoSink>(pipe);
+      }
+    } else {
+      y4m_sink_ = std::make_unique<Y4mVideoSink>(output_file);
+    }
+    client_->RegisterObserver(this);
+  }
+
+  ~Receiver() override { DeletePeerConnection(); }
+
+  // PeerConnectionClientObserver implementation.
+  void OnSignedIn() override {
+    RTC_LOG(LS_INFO) << "Signed in. Waiting for sender to offer...";
+  }
+
+  void OnDisconnected() override {
+    RTC_LOG(LS_INFO) << "Disconnected from signaling server.";
+    DeletePeerConnection();
+    webrtc::Thread::Current()->Quit();
+  }
+
+  void OnPeerConnected(int id, const std::string& name) override {
+    RTC_LOG(LS_INFO) << "Peer connected: " << name << " (id=" << id << ")";
+  }
+
+  void OnPeerDisconnected(int id) override {
+    RTC_LOG(LS_INFO) << "Peer disconnected: " << id;
+    if (id == peer_id_) {
+      DeletePeerConnection();
+      webrtc::Thread::Current()->Quit();
+    }
+  }
+
+  void OnMessageFromPeer(int peer_id, const std::string& message) override {
+    RTC_DCHECK(!message.empty());
+
+    if (!peer_connection_) {
+      RTC_DCHECK(peer_id_ == -1);
+      peer_id_ = peer_id;
+      if (!InitializePeerConnection()) {
+        RTC_LOG(LS_ERROR) << "Failed to initialize PeerConnection.";
+        client_->SignOut();
+        return;
+      }
+    } else if (peer_id != peer_id_) {
+      RTC_LOG(LS_WARNING) << "Message from unknown peer.";
+      return;
+    }
+
+    Json::CharReaderBuilder factory;
+    std::unique_ptr<Json::CharReader> reader =
+        absl::WrapUnique(factory.newCharReader());
+    Json::Value jmessage;
+    if (!reader->parse(message.data(), message.data() + message.length(),
+                       &jmessage, nullptr)) {
+      RTC_LOG(LS_WARNING) << "Received unknown message: " << message;
+      return;
+    }
+
+    std::string type_str;
+    webrtc::GetStringFromJsonObject(
+        jmessage, signaling_helper::kSessionDescriptionTypeName, &type_str);
+
+    if (!type_str.empty()) {
+      auto desc = signaling_helper::ParseSdp(jmessage);
+      if (!desc) {
+        RTC_LOG(LS_ERROR) << "Failed to parse SDP.";
+        return;
+      }
+      peer_connection_->SetRemoteDescription(
+          signaling_helper::DummySetSessionDescriptionObserver::Create().get(),
+          desc.release());
+
+      auto type_maybe = webrtc::SdpTypeFromString(type_str);
+      if (type_maybe && *type_maybe == webrtc::SdpType::kOffer) {
+        peer_connection_->CreateAnswer(
+            this, webrtc::PeerConnectionInterface::RTCOfferAnswerOptions());
+      }
+    } else {
+      auto candidate = signaling_helper::ParseIceCandidate(jmessage);
+      if (candidate) {
+        peer_connection_->AddIceCandidate(candidate.get());
+      }
+    }
+  }
+
+  void OnMessageSent(int err) override {}
+
+  void OnServerConnectionFailure() override {
+    RTC_LOG(LS_ERROR) << "Failed to connect to signaling server.";
+    webrtc::Thread::Current()->Quit();
+  }
+
+  // PeerConnectionObserver implementation.
+  void OnSignalingChange(
+      webrtc::PeerConnectionInterface::SignalingState new_state) override {
+    RTC_LOG(LS_INFO) << "Signaling state: " << new_state;
+  }
+
+  void OnAddTrack(
+      webrtc::scoped_refptr<webrtc::RtpReceiverInterface> receiver,
+      const std::vector<webrtc::scoped_refptr<webrtc::MediaStreamInterface>>&
+          streams) override {
+    RTC_LOG(LS_INFO) << "Track received: " << receiver->id();
+    auto track = receiver->track();
+    if (track &&
+        track->kind() == webrtc::MediaStreamTrackInterface::kVideoKind) {
+      auto* video_track =
+          static_cast<webrtc::VideoTrackInterface*>(track.get());
+      video_track->AddOrUpdateSink(y4m_sink_.get(), webrtc::VideoSinkWants());
+      RTC_LOG(LS_INFO) << "Y4mVideoSink registered on incoming video track.";
+    }
+  }
+
+  void OnRemoveTrack(
+      webrtc::scoped_refptr<webrtc::RtpReceiverInterface> receiver) override {
+    RTC_LOG(LS_INFO) << "Track removed.";
+  }
+
+  void OnDataChannel(
+      webrtc::scoped_refptr<webrtc::DataChannelInterface> channel) override {}
+
+  void OnRenegotiationNeeded() override {}
+
+  void OnIceConnectionChange(
+      webrtc::PeerConnectionInterface::IceConnectionState new_state) override {
+    RTC_LOG(LS_INFO) << "ICE connection state: " << new_state;
+    if (new_state ==
+        webrtc::PeerConnectionInterface::kIceConnectionDisconnected) {
+      RTC_LOG(LS_INFO) << "ICE disconnected. Exiting.";
+      DeletePeerConnection();
+      webrtc::Thread::Current()->Quit();
+    }
+  }
+
+  void OnIceConnectionReceivingChange(bool receiving) override {}
+
+  void OnIceGatheringChange(
+      webrtc::PeerConnectionInterface::IceGatheringState new_state) override {}
+
+  void OnIceCandidate(const webrtc::IceCandidate* candidate) override;
+
+  void OnIceCandidateRemoved(const webrtc::IceCandidate* candidate) override {}
+
+  // CreateSessionDescriptionObserver implementation.
+  void OnSuccess(webrtc::SessionDescriptionInterface* desc) override;
+
+  void OnFailure(webrtc::RTCError error) override {
+    RTC_LOG(LS_ERROR) << ToString(error.type()) << ": " << error.message();
+  }
+
+ private:
+  bool InitializePeerConnection() {
+    webrtc::PeerConnectionFactoryDependencies deps;
+    deps.signaling_thread = webrtc::Thread::Current();
+    deps.env = env_;
+    deps.audio_encoder_factory = webrtc::CreateBuiltinAudioEncoderFactory();
+    deps.audio_decoder_factory = webrtc::CreateBuiltinAudioDecoderFactory();
+    deps.video_encoder_factory =
+        std::make_unique<webrtc::VideoEncoderFactoryTemplate<
+            webrtc::LibvpxVp8EncoderTemplateAdapter,
+            webrtc::LibvpxVp9EncoderTemplateAdapter,
+            webrtc::OpenH264EncoderTemplateAdapter,
+            webrtc::LibaomAv1EncoderTemplateAdapter>>();
+    deps.video_decoder_factory =
+        std::make_unique<webrtc::VideoDecoderFactoryTemplate<
+            webrtc::LibvpxVp8DecoderTemplateAdapter,
+            webrtc::LibvpxVp9DecoderTemplateAdapter,
+            webrtc::OpenH264DecoderTemplateAdapter,
+            webrtc::Dav1dDecoderTemplateAdapter>>();
+    webrtc::EnableMedia(deps);
+    peer_connection_factory_ =
+        webrtc::CreateModularPeerConnectionFactory(std::move(deps));
+
+    if (!peer_connection_factory_) {
+      RTC_LOG(LS_ERROR) << "Failed to create PeerConnectionFactory.";
+      return false;
+    }
+
+    webrtc::PeerConnectionInterface::RTCConfiguration config;
+    config.sdp_semantics = webrtc::SdpSemantics::kUnifiedPlan;
+    webrtc::PeerConnectionInterface::IceServer server;
+    server.uri = GetPeerConnectionString();
+    server.username = GetTurnUserName();
+    server.password = GetTurnPassword();
+    config.servers.push_back(server);
+
+    webrtc::PeerConnectionDependencies pc_dependencies(this);
+    auto error_or_peer_connection =
+        peer_connection_factory_->CreatePeerConnectionOrError(
+            config, std::move(pc_dependencies));
+    if (!error_or_peer_connection.ok()) {
+      RTC_LOG(LS_ERROR) << "Failed to create PeerConnection.";
+      return false;
+    }
+    peer_connection_ = std::move(error_or_peer_connection.value());
+    return true;
+  }
+
+  void DeletePeerConnection() {
+    peer_connection_ = nullptr;
+    peer_connection_factory_ = nullptr;
+    peer_id_ = -1;
+  }
+
+  void SendMessage(const std::string& json_object) {
+    if (peer_id_ == -1)
+      return;
+    while (client_->IsSendingMessage()) {
+      webrtc::Thread::Current()->ProcessMessages(10);
+    }
+    client_->SendToPeer(peer_id_, json_object);
+  }
+
+  int peer_id_ = -1;
+  const webrtc::Environment& env_;
+  PeerConnectionClient* client_;
+  webrtc::scoped_refptr<webrtc::PeerConnectionInterface> peer_connection_;
+  webrtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface>
+      peer_connection_factory_;
+  std::unique_ptr<Y4mVideoSink> y4m_sink_;
+};
+
+void Receiver::OnIceCandidate(const webrtc::IceCandidate* candidate) {
+  Json::Value jmessage = signaling_helper::SerializeIceCandidate(candidate);
+  Json::StreamWriterBuilder factory;
+  SendMessage(Json::writeString(factory, jmessage));
+}
+
+void Receiver::OnSuccess(webrtc::SessionDescriptionInterface* desc) {
+  peer_connection_->SetLocalDescription(
+      signaling_helper::DummySetSessionDescriptionObserver::Create().get(),
+      desc);
+
+  Json::Value jmessage = signaling_helper::SerializeSdp(desc);
+  Json::StreamWriterBuilder factory;
+  SendMessage(Json::writeString(factory, jmessage));
+}
+
+}  // namespace
+
+int main(int argc, char* argv[]) {
+  absl::ParseCommandLine(argc, argv);
+
+  webrtc::Environment env =
+      webrtc::CreateEnvironment(std::make_unique<webrtc::FieldTrials>(
+          absl::GetFlag(FLAGS_force_fieldtrials)));
+
+  int port = absl::GetFlag(FLAGS_port);
+  if (port < 1 || port > 65535) {
+    printf("Error: %i is not a valid port.\n", port);
+    return 1;
+  }
+
+  std::string server = absl::GetFlag(FLAGS_server);
+  if (server.empty()) {
+    printf("Error: --server must be specified.\n");
+    return 1;
+  }
+
+  std::string output_file = absl::GetFlag(FLAGS_output_file);
+  bool play = absl::GetFlag(FLAGS_play);
+
+  std::string peer_name = absl::GetFlag(FLAGS_name);
+  if (peer_name.empty()) {
+    peer_name = GetPeerName();
+  }
+
+  SetupSignalHandler();
+  webrtc::InitializeSSL();
+
+  HeadlessSocketServer socket_server;
+  webrtc::AutoSocketServerThread thread(&socket_server);
+
+  PeerConnectionClient client;
+  auto receiver =
+      webrtc::make_ref_counted<Receiver>(env, &client, output_file, play);
+  client.Connect(server, port, peer_name);
+
+  thread.Run();
+
+  webrtc::CleanupSSL();
+  return 0;
+}
